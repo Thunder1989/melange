15dc3c17e9035d1ac8a1dffe11278aeb419cfcfd
Dezhi Hong <dzhong1989@gmail.com>
2018-01-26 15:34:49 -0500

    cleaned a bit

--- algorithm/transfer_learning.py
+++ algorithm/transfer_learning.py
@@ -230,16 +230,15 @@ class transfer_learning:
 
 if __name__ == "__main__":
 
-    input1 = np.genfromtxt('rice_hour_sdh', delimiter=',')
+    input1 = np.genfromtxt('../data/rice_hour_sdh', delimiter=',')
     #input1 = np.genfromtxt('sdh_hour_soda', delimiter=',')
     #input1 = np.genfromtxt('soda_hour_rice', delimiter=',')
-    input2 = np.genfromtxt('keti_hour_sum', delimiter=',')
+    input2 = np.genfromtxt('../data/keti_hour_sum', delimiter=',')
     #input21 = np.genfromtxt('rice_hour_soda', delimiter=',')
-    input3 = np.genfromtxt('sdh_hour_rice', delimiter=',')
+    input3 = np.genfromtxt('../data/sdh_hour_rice', delimiter=',')
     input2 = np.vstack((input2,input3))
     fd1 = input1[:, 0:-1]
     fd2 = input2[:, 0:-1]
-    fd3 = input3[:, 0:-1]
 
     #self.train_fd = np.hstack((fd1,fd2))
     train_fd = fd1
@@ -248,7 +247,7 @@ if __name__ == "__main__":
     #self.test_label = np.hstack((input2[:,-1],input3[:,-1]))
     test_label = input2[:,-1]
 
-    ptn = [i.strip().split('\\')[-1][:-5] for i in open('rice_pt_sdh').readlines()]
+    ptn = [i.strip().split('\\')[-1][:-5] for i in open('../data/rice_pt_sdh').readlines()]
     test_fn = get_name_features(ptn)
 
     tl = transfer_learning(train_fd, test_fd, train_label, test_label, test_fn, True)
