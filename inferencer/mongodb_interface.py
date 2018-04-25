a6ee6ca5e6351e4812f63c6c8922ba7462c73e49
Karthik Chinnathambi <ckarthik1994@gmail.com>
2018-04-25 14:54:05 -0400

    Adding MongoDB interface and TimeSeries Interface

---
+++ inferencer/mongodb_interface.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import random
+import re
+import itertools
+import pylab as pl
+
+from collections import defaultdict as dd
+from collections import Counter as ct
+
+from sklearn.cluster import KMeans
+from sklearn.mixture import DPGMM
+
+from sklearn.feature_extraction.text import CountVectorizer as CV
+from sklearn.cross_validation import KFold
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix as CM
+from sklearn.preprocessing import normalize
+
+from Inferencer import Inferencer
+
+
+class mongodb_interface():
+
+    def insert_timeseries_data():
+        pass
+
+    def insert_points_data(filename):
+        pass
+
+    def get_points_data(filename):
+        pass
+
+    def recursive_file_read(path):
+        pass
+
---
+++ inferencer/timeseries_interface.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy import stats
+import scipy as sp
+
+from collections import Counter,defaultdict
+from multiprocessing import Pool 
+
+class timeseries_interface():
+# Feature
+def getF_1994_Li(X):
+    pass
+
+# Feature
+def getF_2012_Calbimonte(X,B=20,segs=5):
+    pass
+
+# Feature
+def getF_2015_Gao(X):
+    pass
+
+# Feature
+def getF_2015_Hong(X):
+    pass
+
+
+# Feature
+def getF_2015_Bhattacharya(X):
+    pass
+
+
+# Feature
+def getF_2015_Balaji(X):
+    pass
+
+# Feature
+def getF_2016_Koh(X):
+    pass
