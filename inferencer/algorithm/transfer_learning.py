1fa388985f4dcaea77c44669c545328683e5b340
Dezhi Hong <dzhong1989@gmail.com>
2018-05-03 18:52:52 -0700

    fixed data file path

--- inferencer/algorithm/transfer_learning.py
+++ inferencer/algorithm/transfer_learning.py
@@ -234,12 +234,12 @@ class transfer_learning:
 
 if __name__ == "__main__":
 
-    input1 = np.genfromtxt('../data/rice_hour_sdh', delimiter=',')
+    input1 = np.genfromtxt('../../data/rice_hour_sdh', delimiter=',')
     #input1 = np.genfromtxt('sdh_hour_soda', delimiter=',')
     #input1 = np.genfromtxt('soda_hour_rice', delimiter=',')
-    input2 = np.genfromtxt('../data/keti_hour_sum', delimiter=',')
+    input2 = np.genfromtxt('../../data/keti_hour_sum', delimiter=',')
     #input21 = np.genfromtxt('rice_hour_soda', delimiter=',')
-    input3 = np.genfromtxt('../data/sdh_hour_rice', delimiter=',')
+    input3 = np.genfromtxt('../../data/sdh_hour_rice', delimiter=',')
     input2 = np.vstack((input2,input3))
     fd1 = input1[:, 0:-1]
     fd2 = input2[:, 0:-1]
@@ -251,9 +251,9 @@ if __name__ == "__main__":
     #self.test_label = np.hstack((input2[:,-1],input3[:,-1]))
     test_label = input2[:,-1]
 
-    ptn = [i.strip().split('\\')[-1][:-5] for i in open('../data/rice_pt_sdh').readlines()]
-    self.test_fn = get_name_features(ptn)
+    ptn = [i.strip().split('\\')[-1][:-5] for i in open('../../data/rice_pt_sdh').readlines()]
+    test_fn = get_name_features(ptn)
 
-    tl = transfer_learning(train_fd, test_fd, train_label, test_label, self.test_fn, True)
+    tl = transfer_learning(train_fd, test_fd, train_label, test_label, test_fn, True)
     tl.run_auto()
 
